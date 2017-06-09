38bf4dc1162b789d402b58ed8f2835005e243245
Jonathan Lowell <elderlowell@gmail.com>
2017-06-08 18:42:44 -0600

    Finished miniProfiles3

--- service.js
+++ service.js
@@ -1,10 +1,21 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
+    var deferred = $q.defer();
+    console.log(deferred);
     return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
+      method: 'GET',
+      url: 'https://reqres.in/api/users?page1'
+    }).then(function(response) {
+      var users = response.data.data;
+      users.forEach(function(user) {
+        user.last_name = 'McIver';
+      });
+
+      deferred.resolve(users);
+    });
+    return deferred.promise;
   }
+
 
 });
